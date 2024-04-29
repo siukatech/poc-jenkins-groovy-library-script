5b389e472b658f99828cc5d200981e0aebdce181
siukatech <siuka114@gmail.com>
2024-04-29 18:57:28 +0800

    feat: add other stage and steps

--- vars/nginxStaticReact.groovy
+++ vars/nginxStaticReact.groovy
@@ -30,6 +30,55 @@ pipeline {
                 sh "npm --version"
             }
         }
+        stage('parameter') {
+            steps {
+                echo "[${STAGE_NAME}] config: ${config}"
+                script {
+                    properties([
+                        parameters([
+                            string(
+                                defaultValue: "${config.repoName}", 
+                                name: 'PROJECT_NAME', 
+                                description: 'Project Name',
+                                trim: true
+                            ),
+                            string(
+                                defaultValue: "${config.repoHttp}://${config.repoHostname}/${config.repoUsername}/${config.repoName}.git",
+                                name: 'PROJECT_URL', 
+                                description: 'Project Url',
+                                trim: true
+                            )
+                        ])
+                    ])
+                    // Save to variables. Default to empty string if not found.
+                    env.GITHUB_PROJECT_NAME = params.PROJECT_NAME
+                    env.GITHUB_PROJECT_URL = params.PROJECT_URL
+                }
+                echo "[${STAGE_NAME}] GITHUB_PROJECT_NAME: ${GITHUB_PROJECT_NAME}"
+                echo "[${STAGE_NAME}] GITHUB_PROJECT_URL: ${GITHUB_PROJECT_URL}"
+            }
+        }
+
+        stage('set-up') {
+            steps {
+                sh "npm install --save-dev"
+                sh "npm install"
+            }
+        }
+
+        stage('build') {
+            steps {
+                def skipTest = config.skipTest
+                if (skipTest ) {
+                    sh "npm run build"
+                }
+                else {
+                    sh "npm run build"
+                }
+
+                sh "ls -la build/*"
+            }
+        }
     }
   
     // https://www.jenkins.io/doc/pipeline/tour/post/
